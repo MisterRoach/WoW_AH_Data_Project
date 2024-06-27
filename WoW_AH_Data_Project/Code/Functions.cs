23eb91cb7e472353b471a7274e68d6a8602851ae
MrRoach <renestagis@googlemail.com>
2024-06-27 19:22:29 +0200

    Designed the current "MainWindow.xaml" and it's .cs: - It's possible to choose the path of the TSM AppHelper folder containing the AppData.lua -- It check's if the AppData.lua file exists there or not if not, the button to convert the lua data isn't enabled - It's possible to choose an output path for the generated csv files, if left empty, the path of this file's execution is used - If AppData.lua was found, it's possible to start conversion of the data in it into 2 csv files, one for the regular market values, one for the recent market values - (in future this "MainWindow" shall become the window that's used if user chooses to do that and get's replaced by a new MainWindow)
    ----------------------------------------
    
    Added code folder with .cs files from previous project:
    
    - TSM_Lua_To_csv.cs:
    -- Used to convert the TSM AppHelpers AppData.lua recent and regular AH data into csv files
    
    - HexToDecimal.cs:
    -- Code I found on the web that's used to convert the hex32base values of the AppData.lua into regular decimal values
    
    - Functions.cs:
    -- Contains 2 functions currently:
    --- Function to add text into files
    --- Function for logging

---
+++ WoW_AH_Data_Project/Code/Functions.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Text;
+
+namespace TSM_Data_Converter;
+
+public class Functions
+{
+    // Function to add text into files
+    public static void AddText(FileStream fs, string value)
+    {
+        byte[] info = new UTF8Encoding(true).GetBytes(value);
+        fs.Write(info, 0, info.Length);
+    }
+
+    // Function for logging
+    public static void Log(string file_path, string input)
+    {
+        using (StreamWriter sw = File.AppendText(file_path))
+        {
+            sw.WriteLine(input);
+            sw.Close();
+        }
+    }
+
+}
---
+++ WoW_AH_Data_Project/Code/HexToDecimal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSM_Data_Converter;
+
+// From https://www.geeksforgeeks.org/convert-base-decimal-vice-versa/
+// C# program to convert 
+// a number from any base
+// to decimal
+
+public class GFG
+{
+    // To return value of a char. 
+    // For example, 2 is returned
+    // for '2'. 10 is returned 
+    // for 'A', 11 for 'B'
+    public static int val(char c)
+    {
+        if (c >= '0' && c <= '9')
+            return (int)c - '0';
+        else
+            return (int)c - 'A' + 10;
+    }
+
+    // Function to convert a 
+    // number from given base 
+    // 'b' to decimal
+    public static int toDeci(string str,
+                    int b_ase)
+    {
+        int len = str.Length;
+        int power = 1; // Initialize 
+                       // power of base
+        int num = 0; // Initialize result
+        int i;
+
+        // Decimal equivalent is 
+        // str[len-1]*1 + str[len-2] *
+        // base + str[len-3]*(base^2) + ...
+        for (i = len - 1; i >= 0; i--)
+        {
+            // A digit in input number 
+            // must be less than 
+            // number's base
+            if (val(str[i]) >= b_ase)
+            {
+                Console.WriteLine("Invalid Number");
+                return -1;
+            }
+
+            num += val(str[i]) * power;
+            power = power * b_ase;
+        }
+
+        return num;
+    }
+}
+
+// This code is contributed 
+// by anuj_67.
